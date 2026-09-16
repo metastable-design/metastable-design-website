1d395925069a468850626ba20e0c8a4bc88a79ba
metastable-design <metastable01@gmail.com>
2026-09-16 15:26:17 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,26 @@
 // project, they are not secrets. Access control is enforced by Firebase
 // Auth + Firestore security rules, not by hiding this file.
 
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDN7wHbJmDUFjAPMO8HlnQ1VX7J5ki9zo8",
+    authDomain: "metastable-design-website.firebaseapp.com",
+    projectId: "metastable-design-website",
+    storageBucket: "metastable-design-website.firebasestorage.app",
+    messagingSenderId: "698169227126",
+    appId: "1:698169227126:web:b52854d3f0562c77f36c57",
+    measurementId: "G-7E720JYCC4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
